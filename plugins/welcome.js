66cd3016f18a7b89b7279930d7d2f6cdea7dc75a
GABO3 <gabrielvazquezcivica@gmail.com>
2025-12-12 22:49:38 -0600

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -1,52 +1,50 @@
-let handler = async (m, { conn, participants, action }) => {
-  // ESTE ARCHIVO NO USA handler normal
+let WAMessageStubType = (await import('@whiskeysockets/baileys')).default;
+import fetch from 'node-fetch';
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true;
+
+  let vn = 'https://qu.ax/Jinc.mp3';
+  let vn2 = 'https://qu.ax/ujpr.mp3';
+  let chat = global.db.data.chats[m.chat];
+  const getMentionedJid = () => {
+    return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
+  };
+
+  let who = m.messageStubParameters[0] + '@s.whatsapp.net';
+  let user = global.db.data.users[who];
+
+  let userName = user ? user.name : await conn.getName(who);
+
+ if (chat.welcome && m.messageStubType === 27) {
+    this.sendMessage(m.chat, { audio: { url: vn }, 
+    contextInfo: { forwardedNewsletterMessageInfo: { 
+    newsletterJid: channelRD.id, 
+    serverMessageId: '', 
+    newsletterName: channelRD.name }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
+    "title":`♡︎✿︎𝙱𝚒𝚎𝚗𝚟𝚎𝚗𝚒𝚍𝚘ꨄ︎ఌ︎`, 
+    "body": `${userName}`, 
+    "previewType": "PHOTO", 
+    "thumbnailUrl": null,
+    "thumbnail": icons, 
+    "sourceUrl": redes, 
+    "showAdAttribution": true}}, 
+     seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 }
 
-handler.before = async function (m, { conn, participants, action }) {
-  // 🔒 switch global welcome ON/OFF
-  let chat = global.db.data.chats[m.chat]
-  if (!chat || !chat.welcome) return
-
-  let user = participants[0]
-  let group = await conn.groupMetadata(m.chat)
-  let groupName = group.subject
-  let members = group.participants.length
-
-  let pp = 'https://i.imgur.com/2wzGhpF.png'
-  try {
-    pp = await conn.profilePictureUrl(user, 'image')
-  } catch {}
-
-  // 🔥 BIENVENIDA AGRESIVA
-  if (action === 'add') {
-    let txt = `
-💀 @${user.split('@')[0]}
-Entraste a *${groupName}*
-
-👥 ${members} dentro.
-🩸 Aguanta o desaparece.
-`
-    await conn.sendMessage(m.chat, {
-      image: { url: pp },
-      caption: txt,
-      mentions: [user]
-    })
-  }
-
-  // ☠️ DESPEDIDA AGRESIVA
-  if (action === 'remove') {
-    let txt = `
-⚰️ @${user.split('@')[0]}
-Fuera de *${groupName}*
-
-🚪 Caso cerrado.
-`
-    await conn.sendMessage(m.chat, {
-      image: { url: pp },
-      caption: txt,
-      mentions: [user]
-    })
+  if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
+    this.sendMessage(m.chat, { audio: { url: vn2 }, 
+    contextInfo: { forwardedNewsletterMessageInfo: { 
+    newsletterJid: channelRD.id, 
+    serverMessageId: '', 
+    newsletterName: channelRD.name }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
+    "title": `❀☹︎𝙰𝚍𝚒𝚘𝚜☹︎☯︎`, 
+    "body": `${userName}, se despide.`, 
+    "previewType": "PHOTO", 
+    "thumbnailUrl": null,
+    "thumbnail": icons, 
+    "sourceUrl": redes, 
+    "showAdAttribution": true}}, 
+     seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
   }
 }
-
-export default handler
