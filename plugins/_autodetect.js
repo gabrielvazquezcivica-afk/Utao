51d364197608ebd9135dd8218f3befdac842fa2e
GABO3 <gabrielvazquezcivica@gmail.com>
2025-12-13 21:28:20 -0600

    Update _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -24,7 +24,7 @@ if (chat.detect && m.messageStubType == 25) {
 await this.sendMessage(m.chat, { text: `🚩 *Ahora ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden editar la información del grupo*`, mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 
 } else if (chat.detect && m.messageStubType == 26) {  
-await this.sendMessage(m.chat, { text: `🚩 *El grupo ha sido ${m.messageStubParameters[0] == 'on' ? 'cerrado' : 'abierto'}*\n\n${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden enviar mensajes`,`👤 *Acción realizada por:* ${name}`, `mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})  
+await this.sendMessage(m.chat, { text: `🚩 *El grupo ha sido ${m.messageStubParameters[0] == 'on' ? 'cerrado' : 'abierto'}*\n\n${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden enviar mensajes`,`👤 *Acción realizada por:* ${name},mentions: [m.sender] }, { quoted: fkontak, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})  
   
 } else if (chat.detect && m.messageStubType == 29) {
 let txt1 = `🚩 *Nuevo admin*\n\n`
