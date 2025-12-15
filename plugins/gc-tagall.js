8fa4c6c1a1692b565126ab274a6ef29b5520b089
GABO3 <gabrielvazquezcivica@gmail.com>
2025-12-15 03:08:17 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,34 +1,30 @@
-const handler = async (m, { conn, participants, isAdmin }) => {
-
-  if (!isAdmin)
-    return m.reply('🎅❌ *Ho ho ho…* Solo los **admins del taller de Santa** pueden hacer la mención navideña 🎄✨')
-
-  const emojis = [
-    "🎄","🎅","🤶","🧑‍🎄","🎁","❄️","☃️","⛄","🌟","✨","💫","🕯️",
-    "🔔","🎶","🍪","🥛","🍫","🍬","🍭","🧦","🛷","🦌","🦌","🎀",
-    "🌠","🎆","❤️","💚","🤍","⭐","🌲","🎉","🎊"
-  ];
-
-  const getRandomEmoji = () => emojis[Math.floor(Math.random() * emojis.length)];
-
-  let message = `🎄 *MENCION NAVIDEÑA* 🎅✨\n\n❄️ Que la magia de la Navidad los acompañe ❄️\n\n`;
-
-  for (let mem of participants) {
-    const emoji = getRandomEmoji();
-    message += `${emoji} @${mem.id.split('@')[0]}\n`;
-  }
-
-  conn.sendMessage(
-    m.chat,
-    { text: message, mentions: participants.map(a => a.id) },
-    { quoted: m }
-  );
-};
-
-handler.help = ['tagall'];
-handler.tags = ['group'];
-handler.command = /^(tagall|todos|here)$/i;
-handler.admin = true;
-handler.group = true;
-
+const handler = async (m, { conn, participants }) => {  
+  const emojis = [  
+    "🎄","🎅","🤶","🧑‍🎄","🎁","❄️","☃️","⛄","🌟","✨","💫","🕯️",  
+    "🔔","🎶","🍪","🥛","🍫","🍬","🍭","🧦","🛷","🦌","🦌","🎀",  
+    "🌠","🎆","❤️","💚","🤍","⭐","🌲","🎉","🎊"  
+  ];  
+  
+  const getRandomEmoji = () => emojis[Math.floor(Math.random() * emojis.length)];  
+  
+  let message = `🎄 *MENCION NAVIDEÑA* 🎅✨\n\n❄️ Que la magia de la Navidad los acompañe ❄️\n\n`;  
+  
+  for (let mem of participants) {  
+    const emoji = getRandomEmoji();  
+    message += `${emoji} @${mem.id.split('@')[0]}\n`;  
+  }  
+  
+  conn.sendMessage(  
+    m.chat,  
+    { text: message, mentions: participants.map(a => a.id) },  
+    { quoted: m }  
+  );  
+};  
+  
+handler.help = ['tagall'];  
+handler.tags = ['group'];  
+handler.command = /^(tagall|todos|here)$/i;  
+handler.admin = true;  
+handler.group = true;  
+  
 export default handler;
