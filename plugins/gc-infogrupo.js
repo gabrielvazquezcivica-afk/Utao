6df97b33e50e1f20a38d084baf1f125784ace801
GABO3 <gabrielvazquezcivica@gmail.com>
2025-12-15 03:04:49 -0600

    Update gc-infogrupo.js

--- plugins/gc-infogrupo.js
+++ plugins/gc-infogrupo.js
@@ -1,37 +1,22 @@
-const handler = async (m, { conn, participants, groupMetadata, isAdmin }) => {
+const handler = async (m, { conn, participants, groupMetadata }) => {
 
-  if (!isAdmin)
-    return m.reply('🎅❌ *Ho ho ho…* Solo los **admins del Polo Norte** pueden ver la info secreta del grupo 🎄✨')
+const admins = participants.filter(p => p.admin)
+const owner = groupMetadata.owner || admins[0]?.id
 
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || `${global.icons}`;
-  const {antiToxic, reaction, antiTraba, antidelete, antiviewonce, welcome, detect, antiLink, antiLink2, modohorny, autosticker, audios} = global.db.data.chats[m.chat];
-  const groupAdmins = participants.filter((p) => p.admin);
-  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
-  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+let text = `🎄 *INFO DEL GRUPO*
+👥 Miembros: ${participants.length}
+👑 Owner: @${owner.split('@')[0]}
+`
 
-  const text = `💥 *INFO GRUPO*
-💌 *ID:*
-→ ${groupMetadata.id}
-🥷 *Nombre:*
-→ ${groupMetadata.subject}
-🌟 *Descripción:*
-→ Leelo puta (￣へ ￣ 凸
-💫 *Miembros:*
-→ ${participants.length} Participantes
-👑 *Creador del Grupo:*
-→ @${owner.split('@')[0]}
-🏆 *Administradores:*
-${listAdmin}
+conn.sendMessage(
+m.chat,
+{ text, mentions:[owner] },
+{ quoted:m }
+)
+}
 
-💭 *CONFIGURACIÓN*
+handler.command = ['infogrupo','gp']
+handler.group = true
+handler.admin = true
 
-◈ *Welcome:* ${welcome ? '✅' : '❌'}
-◈ *Detect:* ${detect ? '✅' : '❌'}  
-◈ *Antilink:* ${antiLink ? '✅' : '❌'} 
-◈ *Antilink 𝟸:* ${antiLink2 ? '✅' : '❌'} 
-◈ *Modohorny:* ${modohorny ? '✅' : '❌'} 
-◈ *Autosticker:* ${autosticker ? '✅' : '❌'} 
-◈ *Audios:* ${audios ? '✅' : '❌'} 
-◈ *Antiver:* ${antiviewonce ? '✅' : '❌'} 
-◈ *Reacción* ${reaction ? "✅️" : "❌️"}
-◈ *Delete:* ${antidelete ? '✅' : '❌'}
+export default handler◈ *Delete:* ${antidelete ? '✅' : '❌'}
